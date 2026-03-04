df27ad49d16ba0d5f618928b34d380e556eaa670
Mayur Jade <jademayur@gmail.com>
2026-03-04 19:43:59 +0530

    Add pagination functionality

---
+++ FinVentoryAPI/DTOs/AccountGroupDTOs/PagedRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace FinVentoryAPI.DTOs.AccountGroupDTOs
+{
+    public class PagedRequestDto
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        public string? Search { get; set; }
+
+        public string? SortBy { get; set; } = "GroupName";
+        public string? SortDirection { get; set; } = "asc";
+
+        public int? GroupTypeId { get; set; }
+        public int? BalanceToId { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
---
+++ FinVentoryAPI/DTOs/AccountGroupDTOs/PagedResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FinVentoryAPI.DTOs.AccountGroupDTOs
+{
+    public class PagedResponseDto<T>
+    {
+        public int TotalRecords { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public List<T> Data { get; set; } = new();
+    }
+}
