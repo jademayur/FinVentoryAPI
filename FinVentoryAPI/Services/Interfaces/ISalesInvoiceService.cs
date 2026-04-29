85fac4d5d47be60c01277db0b43d9dfe3853f9bd
Mayur Jade <jademayur@gmail.com>
2026-04-29 16:53:35 +0530

    minor change in Interface

--- FinVentoryAPI/Services/Interfaces/ISalesInvoiceService.cs
+++ FinVentoryAPI/Services/Interfaces/ISalesInvoiceService.cs
@@ -6,18 +6,10 @@ namespace FinVentoryAPI.Services.Interfaces
     public interface ISalesInvoiceService
     {
         Task<SalesInvoiceResponseDto> CreateAsync(CreateSalesInvoiceMainDto dto);
-
         Task<List<SalesInvoiceResponseDto>> GetAllAsync();
-
         Task<SalesInvoiceResponseDto?> GetByIdAsync(int id);
-
         Task<bool> UpdateAsync(int id, UpdateSalesInvoiceMainDto dto);
-
-        Task<bool> DeleteAsync(int id);
-
-       
-
-
+        Task<bool> DeleteAsync(int id); 
         Task<PagedResponseDto<SalesInvoiceResponseDto>> GetPagedAsync(PagedRequestDto request);
     }
 }
